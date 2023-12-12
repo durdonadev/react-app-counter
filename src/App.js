3be542a0f37543cc08918d743c18b0d8a7844e00
Durdona Ibragimova <durdona.dev@gmail.com>
2023-12-11 23:58:34 -0500

    Implement simple count buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 import React from "react";
 import "./App.css";
 
+class Button extends React.Component {
+    // class compnent
+    render() {
+        return (
+            <button onClick={this.props.onClick}>{this.props.children}</button>
+        );
+    }
+}
+
+const Button1 = (props) => {
+    // function component
+    return <button>{props.children}</button>;
+};
+
 class App extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         counter: 0,
+    //         counterBy2: 0
+    //     };
+    // }
+
+    state = {
+        counter: 0,
+        counterBy2: 0
+    };
+
+    handleCounter = (e) => {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    };
+
+    handleCounterBy2 = (e) => {
+        this.setState({
+            counterBy2: this.state.counterBy2 + 2
+        });
+    };
+
     render() {
         return (
             <div>
-                <button
-                    onClick={() => {
-                        alert("Hello");
-                    }}
-                >
-                    Click me
-                </button>
+                <Button onClick={this.handleCounter}>
+                    Click Me {this.state.counter}
+                </Button>
+                <Button onClick={this.handleCounterBy2}>
+                    Click Me {this.state.counterBy2}
+                </Button>
             </div>
         );
     }
